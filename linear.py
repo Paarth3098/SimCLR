0cfa3f6b264b4d7a1f9181c0b729b6f842c50225
Blank_o_O <paarth3098@gmail.com>
2020-04-22 16:34:13 +0530

    Update linear.py

--- linear.py
+++ linear.py
@@ -6,10 +6,10 @@ import torch.nn as nn
 import torch.optim as optim
 from thop import profile, clever_format
 from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
 from tqdm import tqdm
-
-import utils
+from torchvision import transforms
+from torchvision import datasets
+import os
 from model import Model
 
 
@@ -37,8 +37,8 @@ def train_val(net, data_loader, train_optimizer):
 
     total_loss, total_correct_1, total_correct_5, total_num, data_bar = 0.0, 0.0, 0.0, 0, tqdm(data_loader)
     with (torch.enable_grad() if is_train else torch.no_grad()):
-        for data, target in data_bar:
-            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+        for data,_ , target in tqdm(data_loader):
+            data, target = data.to(device), target.to(device)
             out = net(data)
             loss = loss_criterion(out, target)
 
@@ -60,6 +60,32 @@ def train_val(net, data_loader, train_optimizer):
     return total_loss / total_num, total_correct_1 / total_num * 100, total_correct_5 / total_num * 100
 
 
+class SimCLR_Dataset(torch.utils.data.Dataset):
+    # print('Inside wrapper')
+    '''Dataset Wrapper for SimCLR'''
+
+    def __init__(self, data, transform, root, train=True):
+        self.data = data
+        self.transform = transform
+        self.train = train
+        self.root = root
+        self.classes = self._get_classes(self.root)
+
+    def __getitem__(self, index):
+        img, target = self.data[index]
+
+        pos_1 = self.transform(img)
+        pos_2 = self.transform(img)
+
+        return pos_1, pos_2, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_classes(self, dir):
+        classes = [d.name for d in os.scandir(dir)]
+        return classes
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Linear Evaluation')
     parser.add_argument('--model_path', type=str, default='results/128_0.5_200_512_500_model.pth',
@@ -69,26 +95,69 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     model_path, batch_size, epochs = args.model_path, args.batch_size, args.epochs
-    train_data = CIFAR10(root='data', train=True, transform=utils.train_transform, download=True)
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
-    test_data = CIFAR10(root='data', train=False, transform=utils.test_transform, download=True)
-    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    print(device)
+    # train_data = CIFAR10(root='data', train=True, transform=utils.train_transform, download=True)
+    # train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
+    # test_data = CIFAR10(root='data', train=False, transform=utils.test_transform, download=True)
+    # test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
 
-    model = Net(num_class=len(train_data.classes), pretrained_path=model_path).cuda()
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(32),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+    test_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
+    data_dir = '/home/neo/data/paarth_augmentation_tests/simclr/SimCLR-master/data/ptc_dataset'
+    traindir = os.path.join(data_dir, 'train/')
+    valdir = os.path.join(data_dir, 'val/')
+
+    train_dataset = datasets.ImageFolder(traindir)
+    train_dataset = SimCLR_Dataset(train_dataset, transform=train_transform, root=traindir, train=True)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=16,
+        pin_memory=True
+    )
+    test_dataset = datasets.ImageFolder(valdir)
+    test_dataset = SimCLR_Dataset(test_dataset,
+                                  transform=test_transform,
+                                  root=valdir,
+                                  train=False)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=int(batch_size * 0.25),
+        num_workers=16,
+        shuffle=False,
+        pin_memory=True
+    )
+
+    model = Net(num_class=len(train_dataset.classes), pretrained_path=model_path).to(device)
     for param in model.f.parameters():
         param.requires_grad = False
 
-    flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).cuda(),))
+    flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).to(device),))
     flops, params = clever_format([flops, params])
     print('# Model Params: {} FLOPs: {}'.format(params, flops))
     optimizer = optim.Adam(model.fc.parameters(), lr=1e-3, weight_decay=1e-6)
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=0, last_epoch=-1)
     loss_criterion = nn.CrossEntropyLoss()
     results = {'train_loss': [], 'train_acc@1': [], 'train_acc@5': [],
                'test_loss': [], 'test_acc@1': [], 'test_acc@5': []}
 
     best_acc = 0.0
-    for epoch in range(1, epochs + 1):
+    start_epoch = 1
+
+    for epoch in range(start_epoch, epochs + 1):
         train_loss, train_acc_1, train_acc_5 = train_val(model, train_loader, optimizer)
+        # scheduler.step()
         results['train_loss'].append(train_loss)
         results['train_acc@1'].append(train_acc_1)
         results['train_acc@5'].append(train_acc_5)
@@ -98,7 +167,14 @@ if __name__ == '__main__':
         results['test_acc@5'].append(test_acc_5)
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-        data_frame.to_csv('results/linear_statistics.csv', index_label='epoch')
+        data_frame.to_csv('results/iter_1/linear_statistics.csv', index_label='epoch')
+        checkpoint = {
+            'epoch': epoch,
+            'state_dict': model.state_dict(),
+            'best_acc1': best_acc,
+            'optimizer': optimizer.state_dict(),
+        }
+
         if test_acc_1 > best_acc:
             best_acc = test_acc_1
-            torch.save(model.state_dict(), 'results/linear_model.pth')
+            torch.save(checkpoint, 'results/iter_1/linear_model.pth.tar')
